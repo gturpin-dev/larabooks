295de9fd4fc17e42bb3f4a71c6d25294c89e8b30
Guillaume <guillaume.turpin45@gmail.com>
2024-09-03 16:57:13 +0200

    :sparkles: Seed default Users

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create( [
+            'name'     => 'John Doe',
+            'email'    => 'john@doe.fr',
+            'password' => Hash::make('admin')
+        ] );
+    }
+}
